a18e2ee0344fb917121478d5696fcac671edcc5d
awtnb <arikaebanataw@gmail.com>
2025-03-20 11:01:37 +0900

    update builder

--- build.ps1
+++ build.ps1
@@ -1,31 +1,19 @@
-$EXENAME = ($pwd).Path | Split-Path -Leaf
-
-deno compile --allow-import --allow-read --allow-write .\main.ts
-
-if ($LASTEXITCODE -eq 0) {
-    if (Test-Path .\.env) {
-        $d = (Get-Content .\.env -Raw).Trim()
-        if ($d.Length -lt 1) {
-            $d = $env:USERPROFILE | Join-Path -ChildPath "tools\bin"
-        }
-        $d = [System.Environment]::ExpandEnvironmentVariables($d)
-        if (-not (Test-Path $d -PathType Container)) {
-            New-Item -Path $d -ItemType Directory
-        }
-        $n = "{0}.exe" -f $EXENAME
-        if (Test-Path $n) {
-            Get-Item $n | Copy-Item -Destination $d -Force -ErrorAction Stop
-            "COPIED {0} to: {1}" -f $n, $d | Write-Host -ForegroundColor Blue
-        }
-        else {
-            "{0} not found!" -f $n | Write-Host -ForegroundColor Magenta
-        }
-    }
-    else {
-        ".env not found!" | Write-Host -ForegroundColor Magenta
-    }
+$d = $args[0]
+if ($d.length -lt 1) {
+    Write-Host "Specify directory to copy executable."
 }
 else {
-    "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
-}
+    $s = [system.io.path]::DirectorySeparatorChar
+    if (-not $d.EndsWith($s)) {
+        $d += $s
+    }
 
+    deno compile --allow-import --allow-read --allow-write --output=$d .\main.ts
+
+    if ($LASTEXITCODE -eq 0) {
+        "[FINISHED] Executable was built on {0}" -f $d | Write-Host -ForegroundColor Blue
+    }
+    else {
+        "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
+    }
+}
